f6296bde662e1e84e4ae9b503cbf8ddc2da138de
carzoongu <bka1739@gmail.com>
2024-05-27 11:36:46 +0300

    commit repository

---
+++ Main.js
@@ -0,0 +1,32 @@
+const sounds = [
+    "lovedme", "rush", "nolove",
+    "somebody", "lovesong",
+    "sete", "understand", "woman"    
+]
+
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.addEventListener('click', () =>{
+
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    btn.innerText = sound
+
+    document.getElementById('buttons').appendChild(btn)
+})
+
+
+function stopSongs(){
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0
+    })
+}
